19843198e1907fabc4d72a9b91d7924298db33a4
くらりゅ！ <tsutsumitaiyouspww@gmail.com>
2023-12-13 23:12:21 +0900

    Update next.php

--- JSQRtest/next.php
+++ JSQRtest/next.php
@@ -6,11 +6,37 @@ if (!isset($_POST['qr'])  ||  $_POST['qr']===''){
     exit;
 }
 
-$dsn = 'mysql:host=localhost;dbname=dbtest;charset=utf8mb4';
+$dsn      = 'mysql:dbname=junzs_schoolfes ;host=localhost';
+$user     = 'junzs_wp1';
+$password = 'junzssomeyafes';
+
+// DBへ接続
+try{
+    $dbh = new PDO($dsn, $user, $password);
+
+    // クエリの実行
+    $query = "SELECT * FROM queue";
+    $stmt = $dbh->query($query);
+
+    // 表示処理
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        echo $row["name"];
+    }
+
+}catch(PDOException $e){
+    print("データベースの接続に失敗しました".$e->getMessage());
+    die();
+}
+
+// 接続を閉じる
+$dbh = null;
+
+/*$dsn = 'mysql:host=localhost;dbname=dbtest;charset=utf8mb4';
 $username = 'dbtestuser';
 $password = 'dbtest1user';
 $pdo = new PDO($dsn, $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 $st = $pdo->prepare('SELECT * FROM bbs;');
-$st->execute();
+$st->execute();*/
+
 ?>
