eab152d0172e4b5739c7497a94032e452dcc4db6
くらりゅ！ <80697654+kuraryu405@users.noreply.github.com>
2024-01-21 01:08:17 +0900

    okjsdfgg

--- user/queuetime/index.php
+++ user/queuetime/index.php
@@ -8,9 +8,9 @@ try {
 }
 
 // 平均待ち時間を計算
-$sql = "SELECT AVG(TIMESTAMPDIFF(MINUTE, enter, leave)) AS avg_wait_time
+$sql = "SELECT AVG(TIMESTAMPDIFF(MINUTE, enter, leaving)) AS avg_wait_time
         FROM queue
-        WHERE class = '1年1組' AND enter IS NOT NULL AND leave IS NOT NULL";
+        WHERE class = '1年1組' AND enter IS NOT NULL AND leaving IS NOT NULL";
 $result = $pdo->query($sql);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 $extime = $row["avg_wait_time"];
@@ -18,7 +18,7 @@ $extime = $row["avg_wait_time"];
 // 待ち列にいる人数を計算
 $sql = "SELECT COUNT(*) AS queue_count
         FROM queue
-        WHERE class = '1年1組' AND enter IS NULL AND leave IS NULL AND start IS NOT NULL";
+        WHERE class = '1年1組' AND enter IS NULL AND leaving IS NULL AND start IS NOT NULL";
 $result = $pdo->query($sql);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 $queuepeople = $row["queue_count"];
@@ -27,7 +27,7 @@ $queuepeople = $row["queue_count"];
 $queuetime = $extime * $queuepeople;
 
 // 結果を表示
-echo "総待ち時間: " . $queuetime . " 分";
+echo  '1年1組の待ち時間は'. $queuetime . '分';
 
 // データベース接続の終了
 $pdo = null;
