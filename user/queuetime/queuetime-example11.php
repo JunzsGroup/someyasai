91a63edd2192fbe3fc62c1b7014de74777301a37
くらりゅ！ <tsutsumitaiyouspww@gmail.com>
2024-01-22 20:55:27 +0900

    Update queuetime-example11.php

--- user/queuetime/queuetime-example11.php
+++ user/queuetime/queuetime-example11.php
@@ -6,29 +6,136 @@ try {
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
-
-// 平均待ち時間を計算
-$sql = "SELECT AVG(TIMESTAMPDIFF(MINUTE, enter, leaving)) AS avg_wait_time
-        FROM queue
-        WHERE class = '1年1組' AND enter IS NOT NULL AND leaving IS NOT NULL";
+$sql = <<<EOQ
+SELECT username, T.avg_enjoy_time
+from junzs_login.users left join (
+    SELECT class, AVG(TIMESTAMPDIFF(MINUTE, enter, leaving)) AS avg_enjoy_time
+        FROM junzs_schoolfes.queue WHERE enter IS NOT NULL AND leaving IS NOT NULL
+        GROUP BY class
+    ) as T
+    on junzs_login.users.username = T.class;
+EOQ;
 $result = $pdo->query($sql);
-$row = $result->fetch(PDO::FETCH_ASSOC);
-$extime = $row["avg_wait_time"];
+$extimes = $result->fetchAll(PDO::FETCH_ASSOC);
 
-// 待ち列にいる人数を計算
-$sql = "SELECT COUNT(*) AS queue_count
+
+/*$sql = <<<SQL
+SELECT COUNT(*) AS queue_people_count
         FROM queue
-        WHERE class = '1年1組' AND enter IS NULL AND leaving IS NULL AND start IS NOT NULL";
+        WHERE  enter IS NULL AND leaving IS NULL AND start IS NOT NULL
+        GROUP BY class;
+SQL;
 $result = $pdo->query($sql);
-$row = $result->fetch(PDO::FETCH_ASSOC);
-$queuepeople = $row["queue_count"];
+$queuepeoples = $result->fetchAll(PDO::FETCH_ASSOC);
+*/
 
-// 総待ち時間を計算
-$queuetime = $extime * $queuepeople;
+$sql = <<<queti
+SELECT username, Q.queue_people_count, T.avg_enjoy_time * Q.queue_people_count AS queuetime
+FROM junzs_login.users LEFT JOIN (
+    SELECT class, AVG(TIMESTAMPDIFF(MINUTE, enter, leaving)) AS avg_enjoy_time
+    FROM junzs_schoolfes.queue WHERE enter IS NOT NULL AND leaving IS NOT NULL
+    GROUP BY class
+) AS T ON junzs_login.users.username = T.class
+LEFT JOIN (
+    SELECT class, COUNT(*) AS queue_people_count
+    FROM queue
+    WHERE enter IS NULL AND leaving IS NULL AND start IS NOT NULL
+    GROUP BY class
+) AS Q ON junzs_login.users.username = Q.class;
 
-// 結果を表示
-echo  '1年1組の待ち時間は'. $queuetime . '分';
+queti;
+$result = $pdo->query($sql);
+$queuetimes = $result->fetchAll(PDO::FETCH_ASSOC);
 
-// データベース接続の終了
+$sql = <<<WPURL
+SELECT username, url AS wpurls
+FROM users;
+WPURL;
+$result = $pdo->query($sql);
+$rows = $result->fetchAll(PDO::FETCH_ASSOC);
+$wpurls = array_column($rows, 'wpurls', 'username');
+//var_dump($wpurls);
+
+
+
+//$queuetime = $extime * $queuepeople;
+//ここに総待ち時間の計算
+
+//echo  '1年1組の待ち時間は'. $queuetime . '分';
+
+
 $pdo = null;
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        ul.cardlist {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+        ul.cardlist >li {
+            margin: 40px 1em;
+            padding: 0;
+
+        }
+        ul.cardlist > li > a {
+            border: 1px solid black;
+            border-radius: 8px;
+            display: flex;
+            flex-flow: row nowrap;
+            justify-content: stretch;
+            text-decoration: none;
+            color: black;
+
+        }
+
+        ul.cardlist > li > a >img{
+            width: 150px;
+            height: 150px;
+            object-fit: cover;
+        }    
+
+        .content {
+            margin-left: 20px;
+        }
+        .classname {
+            font-size: 1.5em;
+            font-weight: bold;
+            margin-top: 0;
+        }
+        .waittime {
+            margin-top: 10px;
+        }
+        .waitpeople {
+            margin-top: 5px;
+        }
+      </style>
+    <title>待ち時間</title>
+</head>
+<body>
+
+    <ul class="cardlist">
+        
+<?php foreach ($queuetimes as $queuetime){ ?>
+
+    <li>
+    <a href="<?= $wpurls[$queuetime['username']] ?>">
+            <img src="https://placehold.jp/600x400.png" alt="">
+            <div class="content">
+            <p class="classname"><?= $queuetime['username'] ?></p>
+            <p class="waittime">待ち時間<?= is_null($queuetime['queuetime']) ? '0' : intval($queuetime['queuetime']) ?>分</p>
+            <P class="waitpeople">待ち人数<?= is_null($queuetime['queue_people_count']) ? '0' : $queuetime['queue_people_count'] ?>人</P>
+            </div>
+        </a>
+   </li>
+
+<?php } ?>
+
+    </ul>
+
+</body>
+</html>
