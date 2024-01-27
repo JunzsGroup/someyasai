4df382ce56bbfda1a7b9c98c0c7b4f5d22841c19
くらりゅ！ <80697654+kuraryu405@users.noreply.github.com>
2024-01-27 17:27:38 +0900

    Update reservation.php

--- someyafes/user/reserve/reservation.php
+++ someyafes/user/reserve/reservation.php
@@ -14,26 +14,36 @@ try {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $customerid = $_POST['customerid'];
-    $class = $_POST['class'];
-    $time = $_POST['enter_time'];
+    if (isset($_POST['customerid'])) {
+        $customerid = $_POST['customerid'];
+        $class = $_POST['class'];
+        $time = $_POST['enter_time'];
 
-    $sql = "INSERT INTO reserve (id, class, time) VALUES (:customerid, :class, :time)";
-    $stmt = $PDO->prepare($sql);
-    $stmt->bindParam(':customerid', $customerid, PDO::PARAM_STR);
-    $stmt->bindParam(':class', $class, PDO::PARAM_STR);
-    $stmt->bindParam(':time', $time, PDO::PARAM_STR);
-    $stmt->execute();
+        $sql = "INSERT INTO reserve (id, class, time) VALUES (:customerid, :class, :time)";
+        $stmt = $PDO->prepare($sql);
+        $stmt->bindParam(':customerid', $customerid, PDO::PARAM_STR);
+        $stmt->bindParam(':class', $class, PDO::PARAM_STR);
+        $stmt->bindParam(':time', $time, PDO::PARAM_STR);
+        $stmt->execute();
+    } else {
+        $message = "こちらにアクセスしたあとに予約してください。到着時に本人確認ができなくなります";
+    }
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>予約</title>
 </head>
 <body>
-    <h2><?php echo $class; ?>で<?php echo $time; ?>の時間にお待ちしております。入場の際はQRコードを提示してください。</h2>
+    <?php
+    if (isset($message)) {
+        echo "<h2>$message</h2>";
+    } else {
+        echo "<h2>{$class}で{$time}の時間にお待ちしております。入場の際はQRコードを提示してください。</h2>";
+    }
+    ?>
 </body>
 </html>
