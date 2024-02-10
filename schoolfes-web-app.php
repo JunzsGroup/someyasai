ed4a1540c981b080c9bd477a65307ee269e3003e
くらりゅ！ <tsutsumitaiyouspww@gmail.com>
2024-02-10 12:20:27 +0900

    わあああ

---
+++ schoolfes-web-app.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>製作中</title>
+    <style>
+    .logo {
+    width: 30px; /* ロゴのサイズを調整 */
+    height: auto;
+}
+    </style>
+    <div class="logo">
+    <img src="logotext.jpg">
+    </div>
+</head>
+<body>
+   <h1> 現在制作中です！！<h1>
+    <P>ごめんなさい！！<p>
+</body>
+</html>
